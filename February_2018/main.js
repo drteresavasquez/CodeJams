a47be6d84ca3d760d3773732d100cbd880552f61
Trinity Terry <trinityterry@trinsmac.local>
2018-02-10 15:57:43 -0600

    got rid of commented code

--- February_2018/main.js
+++ February_2018/main.js
@@ -1,36 +1,13 @@
-/* 
-2/10/2018 
-*/
-
-//Get the names from the User
-//Randomly select name
-//Present selected name to User
-
-// function load(){
-//     $("#output").html(
-//     ` <div class="form-group">
-//     <label for="exampleFormControlTextarea1">Example textarea</label>
-//     <textarea class="form-control" id="nameInput" rows="3"></textarea>
-// </div>
-// <button type="button" class="btn btn-success" id="nameSubmit">Submit</button>`
-// )
-// }
-
-$("#nameSubmit").click(function submitNames(){
-let nameString = $("#nameInput").val();
-let nameArray = nameString.split(",");
-let randomName = nameArray[Math.floor(Math.random()*nameArray.length)];
-$("#output").html(
-    `<h3>${randomName}</h3>
+$("#nameSubmit").click(function submitNames() {
+    let nameString = $("#nameInput").val();
+    let nameArray = nameString.split(",");
+    let randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
+    $("#output").html(
+        `<h3>${randomName}</h3>
     <button type="button" class="btn btn-success" id="back">Go Back</button>`
-)
+    )
 
-$("#back").click(function (){
-    location.reload();
+    $("#back").click(function () {
+        location.reload();
+    })
 })
-})
-
-// load()
-
-
-// trinity, remle, gabrielle, olivia
